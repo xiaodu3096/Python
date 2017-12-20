298d9f515348a3574b9056e6c020a68660c12c00
xiaodu3096 <309625528@qq.com>
2017-12-20 20:40:10 +0800

    回顾

---
+++ Demo2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/20 20:36
+# @Author  : DaBai
+# @Site    : 
+# @File    : Demo2.py
+# @Software: PyCharm
+
+greeting = 'Hello'
+if greeting == 'Hello':
+    print('你好')
+else:
+    print('出错了')
