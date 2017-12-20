cc70f60b695a8404a88872d6068580ffc38c7484
xiaodu3096 <309625528@qq.com>
2017-12-20 23:11:34 +0800

    测试pass的用法

--- demo1.py
+++ demo1.py
@@ -1,9 +1,17 @@
-names = ['xiaomeng','xiaoming']
-for name in names:
-    if  name == 'xiao':
-        print('名称:',name)
-        break
-    print('循环名称列表' + name)
+# names = ['xiaomeng','xiaoming']
+# for name in names:
+#     if  name == 'xiao':
+#         print('名称:',name)
+#         break
+#     print('循环名称列表' + name)
+# else:
+#         print('没有循环数据')
+# print('循环结束')
+name = 'xiaomeng'
+if name == 'xiaomeng':
+    print('Hello')
+elif name == 'xiaozhi':
+    #不做任何处理
+    pass
 else:
-        print('没有循环数据')
-print('循环结束')
+    print('nothing')
