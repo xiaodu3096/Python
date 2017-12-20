cd8d480f74d334b9ea61450af396fc30d26a7eed
xiaodu3096 <309625528@qq.com>
2017-12-20 23:05:45 +0800

    测试FOR循环中Break的用法

--- demo1.py
+++ demo1.py
@@ -1,1 +1,9 @@
-print('!!@@@@@!')
+names = ['xiaomeng','xiaoming']
+for name in names:
+    if  name == 'xiao':
+        print('名称:',name)
+        break
+    print('循环名称列表' + name)
+else:
+        print('没有循环数据')
+print('循环结束')
