3df407ebd17e426ef0915f192341f7b1c0120340
xiaodu3096 <309625528@qq.com>
2017-12-21 21:36:30 +0800

    1000以内阿姆斯特朗数的算法

--- demo1.py
+++ demo1.py
@@ -7,11 +7,50 @@
 # else:
 #         print('没有循环数据')
 # print('循环结束')
-name = 'xiaomeng'
-if name == 'xiaomeng':
-    print('Hello')
-elif name == 'xiaozhi':
-    #不做任何处理
-    pass
-else:
-    print('nothing')
+# name = 'xiaomeng'
+# if name == 'xiaomeng':
+#     print('Hello')
+# elif name == 'xiaomeng':
+#     #不做任何处理
+#     pass
+# else:
+#     print('nothing')
+
+# num = 10
+# for nums in range(11):
+#     if nums > 8:
+#         print('当前的数字是：',nums)
+#     else:
+#         continue
+#
+# number = 10
+# while number > 0:
+#     print('当前的数字是：', number)
+#     number-=1
+#     if number == 8:
+#         break
+#
+for nums in range(1000):
+    if 0 < nums < 10:
+        if nums**1 == nums:
+            print(nums)
+            continue
+    elif  10 <= nums <= 99:
+        if int(nums / 10)**2 + (nums % 10)**2 == nums:
+            print(nums)
+            continue
+    elif 100 <= nums <= 1000:
+        if int(nums / 100)**3 + int((nums % 100)/10)**3 +int((nums % 100)%10)**3 == nums:
+            print(nums)
+            continue
+# 153
+
+#
+# nums = 153
+# print(int((nums % 100)%10))
+#
+# print(int(nums / 100)**3 + int((nums % 100)/10)**3 +int((nums % 100)%10)**3)
+# nums = 153
+# print(int((nums % 100)%10))
+#
+# print(int(nums / 100)**3 + int((nums % 100)/10)**3 +int((nums % 100)%10)**3)
