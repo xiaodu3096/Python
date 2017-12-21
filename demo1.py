5bc650d30704e3aa7eb4b67ef3528af963593bc7
xiaodu3096 <309625528@qq.com>
2017-12-21 21:48:45 +0800

    1000以内阿姆斯特朗数的算法

--- demo1.py
+++ demo1.py
@@ -29,21 +29,63 @@
 #     number-=1
 #     if number == 8:
 #         break
+
+# print('for循环方式算法')
+# number = input('请输入数字：')
+# for nums in range(1000):
+#     if 0 < nums < 10:
+#         if nums**1 == nums:
+#             print(nums)
+#             continue
+#     elif  10 <= nums <= 99:
+#         if int(nums / 10)**2 + (nums % 10)**2 == nums:
+#             print(nums)
+#             continue
+#     elif 100 <= nums <= 1000:
+#         if int(nums / 100)**3 + int((nums % 100)/10)**3 +int((nums % 100)%10)**3 == nums:
+#             print(nums)
+#             continue
 #
-for nums in range(1000):
-    if 0 < nums < 10:
-        if nums**1 == nums:
-            print(nums)
-            continue
-    elif  10 <= nums <= 99:
-        if int(nums / 10)**2 + (nums % 10)**2 == nums:
-            print(nums)
-            continue
-    elif 100 <= nums <= 1000:
-        if int(nums / 100)**3 + int((nums % 100)/10)**3 +int((nums % 100)%10)**3 == nums:
-            print(nums)
-            continue
+
+
+nums = int(input('请输入数字：'))
+if 0 < nums < 10:
+    if nums**1 == nums:
+        print('输入的数字是阿斯特朗数')
+    else:
+        print('输入的数字不是阿斯特朗数')
+elif  10 <= nums <= 99:
+    if int(nums / 10)**2 + (nums % 10)**2 == nums:
+        print('输入的数字是阿斯特朗数')
+    else:
+        print('输入的数字不是阿斯特朗数')
+elif 100 <= nums <= 1000:
+    if int(nums / 100)**3 + int((nums % 100)/10)**3 +int((nums % 100)%10)**3 == nums:
+        print('输入的数字是阿斯特朗数')
+    else:
+        print('输入的数字不是阿斯特朗数')
+
+
 # 153
+# print('While方式算法')
+#
+# number = 1000
+# while number > 0:
+#     if 0 < number < 10:
+#         if number ** 1 == number:
+#             print(number)
+#             number -= 1
+#             break
+#     elif 10 <= number <= 99:
+#         if int(number / 10) ** 2 + (number % 10) ** 2 == number:
+#             print(number)
+#             number -= 1
+#             break
+#     elif 100 <= number <= 1000:
+#         if int(number / 100) ** 3 + int((number % 100) / 10) ** 3 + int((number % 100) % 10) ** 3 == number:
+#             print(number)
+#             number -= 1
+#             break
 
 #
 # nums = 153
